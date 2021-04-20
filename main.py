7edf4fb46ae32b26692daf4644f23a07b433964e
macos <macos@macoss-MacBook-Air.local>
2021-04-20 22:53:43 +0400

    delete additional comments

--- main.py
+++ main.py
@@ -20,22 +20,3 @@ if __name__ == "__main__":
     # LEARN
     # learnEnvironment(connection, w, p)
     print(connection.get_my_teams_rl_score(teamId))
-
-    # stateDict = w.get_stateDict()
-    # for k in stateDict:
-    # # k = 0
-    #     st = stateDict[k]
-    #     print(st.get_exploredActions())
-    #     print(st.getReward())
-    #     print("W")
-    #     print(st.get_actionQvalue(1))
-    #     print("E")
-    #     print(st.get_actionQvalue(2))
-    #     print("N")
-    #     print(st.get_actionQvalue(3))
-    #     print("S")
-    #     print(st.get_actionQvalue(4))
-    #     print("#####################")
-    #resp = connection.get_my_team_last_x_runs(1248, 1)
-    #print(resp)
-    
